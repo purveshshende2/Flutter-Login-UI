2f438697019f81c5d340c7cf562e3c1eacffd4c8
Purvesh Shende <56558717+purveshshende2@users.noreply.github.com>
2020-11-26 00:26:31 +0530

    part one of login ui

--- lib/main.dart
+++ lib/main.dart
@@ -32,67 +32,92 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   int _PageState = 0; // 0 for page 1 , 1 for page2 .....
 
+  var _backgroundColor = Colors.white;
+  var _headingColor = Color(0xFFB583d72);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    switch (_PageState) {
+      case 0:
+        _backgroundColor = Colors.white;
+        _headingColor = Color(0xFFB583d72);
+        break;
+      case 1:
+        _backgroundColor = Color(0xFFBfd8c04);
+        _headingColor = Colors.white;
+        break;
+      case 2:
+        _backgroundColor = Color(0xFFBfd8c04);
+        _headingColor = Colors.white;
+    }
+
+    return AnimatedContainer(
+        curve: Curves.fastLinearToSlowEaseIn,
+        duration: Duration(milliseconds: 1000),
+        color: _backgroundColor,
         child: Column(
-      mainAxisAlignment:
-          MainAxisAlignment.spaceBetween, //for spacing in these containers
-      children: <Widget>[
-        Container(
-            child: Column(
-          children: [
+          mainAxisAlignment:
+              MainAxisAlignment.spaceBetween, //for spacing in these containers
+          children: <Widget>[
             Container(
-              margin: const EdgeInsets.only(top: 100),
-              child: Text(
-                "Meet Free",
-                style: TextStyle(
-                    color: Color(0xFFB583d72),
-                    fontSize: 29,
-                    fontWeight: FontWeight.bold),
+                child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 100),
+                  child: Text(
+                    "Meet Free",
+                    style: TextStyle(
+                        color: _headingColor,
+                        fontSize: 29,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                    margin: EdgeInsets.all(20),
+                    padding: EdgeInsets.symmetric(horizontal: 32),
+                    child: Text(
+                      "Join app for free and attain & host your meeting for free",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: _headingColor, fontSize: 18),
+                    ))
+              ],
+            )),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 32),
+              child: Center(
+                child: Image.asset("assets/images/one.jpg"), //Image
               ),
             ),
             Container(
-                margin: EdgeInsets.all(32),
-                padding: EdgeInsets.symmetric(horizontal: 32),
-                child: Text(
-                  "Join app for free and attain & host your meeting for free",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Color(0xFFB583d72), fontSize: 18),
-                ))
-          ],
-        )),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 32),
-          child: Center(
-            child: Image.asset("assets/images/one.jpg"), //Image
-          ),
-        ),
-        Container(
-            child: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    _PageState = 1;
-                  });
-                },
-                child: Container(
-                  child: Center(
+                child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        if (_PageState != 0) {
+                          _PageState = 0;
+                        } else {
+                          _PageState = 1;
+                        }
+                      });
+                    },
                     child: Container(
-                        margin: EdgeInsets.all(35),
-                        padding: EdgeInsets.all(20),
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                            color: Color(0xFFB583d72),
-                            borderRadius: BorderRadius.circular(50)),
-                        child: Center(
-                          child: Text(
-                            "Get Started",
-                            style: TextStyle(color: Colors.white, fontSize: 15),
-                          ),
-                        )), //Button
-                  ),
-                )))
-      ],
-    ));
+                      child: Center(
+                        child: Container(
+                            margin: EdgeInsets.all(35),
+                            padding: EdgeInsets.all(20),
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                                color: Color(0xFFB583d72),
+                                borderRadius: BorderRadius.circular(50)),
+                            child: Center(
+                              child: Text(
+                                "Get Started",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 15),
+                              ),
+                            )), //Button
+                      ),
+                    )))
+          ],
+        ));
   }
 }
