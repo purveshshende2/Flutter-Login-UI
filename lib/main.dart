c89fa6dde2f5a8e8f43e64a556b484a8538414d9
Purvesh Shende <56558717+purveshshende2@users.noreply.github.com>
2020-12-04 23:33:52 +0530

    adding main flutter design part

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:keyboard_visibility/keyboard_visibility.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,18 +11,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(fontFamily: "Lato"
-            //for whole app font is Lato for single container
-            // just use style : TextStyle(
-            //fontFamily : "here"
-            //)
-            ),
-        debugShowCheckedModeBanner: false, //for hide debug banner
-        home: Scaffold(
-          body: Container(
-            child: LoginPage(),
-          ),
-        ));
+      theme: ThemeData(fontFamily:"Lato"),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Container(
+          child: LoginPage(),
+        ),
+      ),
+    );
   }
 }
 
@@ -30,206 +29,207 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  int _PageState = 0; // 0 for page 1 , 1 for page2 .....
+  int _pageState = 0;
 
   var _backgroundColor = Colors.white;
-  var _headingColor = Color(0xFFB583d72);
+  var _headingColor = Color(0xFFB40284A);
 
   double _headingTop = 100;
+
   double _loginWidth = 0;
+  double _loginHeight = 0;
+  double _loginOpacity = 1;
+
   double _loginYOffset = 0;
-  double _registerYOffset = 0;
-
   double _loginXOffset = 0;
+  double _registerYOffset = 0;
+  double _registerHeight = 0;
 
-  double windowWidth = 0; //I get container over there
+  double windowWidth = 0;
   double windowHeight = 0;
 
-  double _loginOpacity = 1;
+  bool _keyboardVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    KeyboardVisibilityNotification().addNewListener(
+      onChange: (bool visible) {
+        setState(() {
+          _keyboardVisible = visible;
+          print("Keyboard State Changed : $visible");
+        });
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     windowHeight = MediaQuery.of(context).size.height;
     windowWidth = MediaQuery.of(context).size.width;
 
-    switch (_PageState) {
+    _loginHeight = windowHeight - 270;
+    _registerHeight = windowHeight - 270;
+
+    switch (_pageState) {
       case 0:
         _backgroundColor = Colors.white;
-        _headingColor = Color(0xFFB583d72);
-        _loginYOffset = windowHeight;
-        _registerYOffset = windowHeight;
-        _loginXOffset = 0;
+        _headingColor = Color(0xFFB583D72);
+
+        _headingTop = 100;
+
         _loginWidth = windowWidth;
         _loginOpacity = 1;
-        _headingTop = 100;
+
+        _loginYOffset = windowHeight;
+        _loginHeight = _keyboardVisible ? windowHeight : windowHeight - 270;
+
+        _loginXOffset = 0;
+        _registerYOffset = windowHeight;
         break;
       case 1:
-        _backgroundColor = Color(0xFFBfd8c04);
+        _backgroundColor = Color(0xFFB34626C);
         _headingColor = Colors.white;
-        _loginYOffset = 270;
-        _registerYOffset = windowHeight;
-        _loginXOffset = 0;
+
+        _headingTop = 90;
+
         _loginWidth = windowWidth;
         _loginOpacity = 1;
-        _headingTop = 90;
+
+        _loginYOffset = _keyboardVisible ? 40 : 270;
+        _loginHeight = _keyboardVisible ? windowHeight : windowHeight - 270;
+
+        _loginXOffset = 0;
+        _registerYOffset = windowHeight;
         break;
       case 2:
-        _backgroundColor = Color(0xFFBfd8c04);
+        _backgroundColor = Color(0xFFB34626C);
         _headingColor = Colors.white;
-        _loginYOffset = 250;
-        _registerYOffset = 270;
-        _loginXOffset = 20;
+
+        _headingTop = 80;
+
         _loginWidth = windowWidth - 40;
         _loginOpacity = 0.7;
-        _headingTop = 80;
+
+        _loginYOffset = _keyboardVisible ? 30 : 240;
+        _loginHeight = _keyboardVisible ? windowHeight : windowHeight - 240;
+
+        _loginXOffset = 20;
+        _registerYOffset = _keyboardVisible ? 55 : 270;
+        _registerHeight = _keyboardVisible ? windowHeight : windowHeight - 270;
+        break;
     }
-    return Stack(children: <Widget>[
-      AnimatedContainer(
-          curve: Curves.fastLinearToSlowEaseIn,
-          duration: Duration(milliseconds: 1000),
-          color: _backgroundColor,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment
-                .spaceBetween, //for spacing in these containers
-            children: <Widget>[
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    _PageState = 0;
-                  });
-                },
-                child: AnimatedContainer(
-                    curve: Curves.fastLinearToSlowEaseIn,
-                    duration: Duration(milliseconds: 1000),
+
+    return Stack(
+      children: <Widget>[
+        AnimatedContainer(
+            curve: Curves.fastLinearToSlowEaseIn,
+            duration: Duration(milliseconds: 1000),
+            color: _backgroundColor,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _pageState = 0;
+                    });
+                  },
+                  child: Container(
                     child: Column(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.only(top: _headingTop),
+                      children: <Widget>[
+                        AnimatedContainer(
+                          curve: Curves.fastLinearToSlowEaseIn,
+                          duration: Duration(milliseconds: 1000),
+                          margin: EdgeInsets.only(
+                            top: _headingTop,
+                          ),
                           child: Text(
-                            "Meet Free",
-                            style: TextStyle(
-                                color: _headingColor,
-                                fontSize: 29,
-                                fontWeight: FontWeight.bold),
+                            "MeetFree",
+                            style:
+                                TextStyle(color: _headingColor, fontSize: 28),
                           ),
                         ),
                         Container(
-                            margin: EdgeInsets.all(25),
-                            padding: EdgeInsets.symmetric(horizontal: 32),
-                            child: Text(
-                              "Join MeetFree app for free and attain & host your meeting for free",
-                              textAlign: TextAlign.center,
-                              style:
-                                  TextStyle(color: _headingColor, fontSize: 18),
-                            ))
+                          margin: EdgeInsets.all(20),
+                          padding: EdgeInsets.symmetric(horizontal: 32),
+                          child: Text(
+                            "Join MeetFree app for free and attain & host your meeting for free",
+                            textAlign: TextAlign.center,
+                            style:
+                                TextStyle(color: _headingColor, fontSize: 16),
+                          ),
+                        )
                       ],
-                    )),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 32),
-                child: Center(
-                  child: Image.asset("assets/images/one.jpg"), //Image
+                    ),
+                  ),
                 ),
-              ),
-              Container(
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 32),
+                  child: Center(
+                    child: Image.asset("assets/images/one.jpg"),
+                  ),
+                ),
+                Container(
                   child: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          if (_PageState != 0) {
-                            _PageState = 0;
-                          } else {
-                            _PageState = 1;
-                          }
-                        });
-                      },
-                      child: Container(
-                        child: Center(
-                          child: Container(
-                              margin: EdgeInsets.all(35),
-                              padding: EdgeInsets.all(25),
-                              width: double.infinity,
-                              decoration: BoxDecoration(
-                                  color: Color(0xFFB583d72),
-                                  borderRadius: BorderRadius.circular(50)),
-                              child: Center(
-                                child: Text(
-                                  "Get Started",
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 15),
-                                ),
-                              )), //Button
+                    onTap: () {
+                      setState(() {
+                        if (_pageState != 0) {
+                          _pageState = 0;
+                        } else {
+                          _pageState = 1;
+                        }
+                      });
+                    },
+                    child: Container(
+                      margin: EdgeInsets.all(32),
+                      padding: EdgeInsets.all(20),
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          color: Color(0xFFB583D72), // btn color
+                          borderRadius: BorderRadius.circular(50)),
+                      child: Center(
+                        child: Text(
+                          "Get Started",
+                          style: TextStyle(color: Colors.white, fontSize: 16),
                         ),
-                      )))
-            ],
-          )),
-      GestureDetector(
-        onTap: () {
-          setState(() {
-            _PageState = 2;
-          });
-        },
-        child: AnimatedContainer(
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )),
+        AnimatedContainer(
           padding: EdgeInsets.all(32),
           width: _loginWidth,
+          height: _loginHeight,
           curve: Curves.fastLinearToSlowEaseIn,
           duration: Duration(milliseconds: 1000),
-          //---------------------------------------------------------------------
-          //This container for transforming
           transform: Matrix4.translationValues(_loginXOffset, _loginYOffset, 1),
           decoration: BoxDecoration(
               color: Colors.white.withOpacity(_loginOpacity),
               borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(25), topRight: Radius.circular(25))),
           child: Column(
-            children: <Widget>[
-              PrimaryButton(
-                btnText: "Login",
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              OutlineBtn(
-                btnText: "Create new account",
-              )
-            ],
-          ),
-        ),
-      ),
-      GestureDetector(
-        onTap: () {
-          setState(() {
-            _PageState = 1;
-          });
-        },
-        child: AnimatedContainer(
-          padding: EdgeInsets.all(32),
-          curve: Curves.fastLinearToSlowEaseIn,
-          duration: Duration(milliseconds: 1000),
-          //---------------------------------------------------------------------
-          //This container for transforming
-          transform: Matrix4.translationValues(0, _registerYOffset, 1),
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(25), topRight: Radius.circular(25))),
-
-          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Column(
-                children: [
+                children: <Widget>[
                   Container(
-                    margin:EdgeInsets.only(bottom:20),
-                    child: Text("create a new account",
-                      style : TextStyle(
-                        fontSize: 20,
-                      )
+                    margin: EdgeInsets.only(bottom: 20),
+                    child: Text(
+                      "Login To Continue",
+                      style: TextStyle(fontSize: 20),
                     ),
                   ),
                   InputWithIcon(
                     icon: Icons.email,
                     hint: "Enter Email...",
                   ),
-                  SizedBox(height: 20,),
+                  SizedBox(
+                    height: 20,
+                  ),
                   InputWithIcon(
                     icon: Icons.vpn_key,
                     hint: "Enter Password...",
@@ -237,32 +237,136 @@ class _LoginPageState extends State<LoginPage> {
                 ],
               ),
               Column(
-                children: [
+                children: <Widget>[
                   PrimaryButton(
-                    btnText: "Create Account", //text for sign up
+                    btnText: "Login",
                   ),
                   SizedBox(
                     height: 20,
                   ),
-                  OutlineBtn(
-                    btnText: "Back to login",
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _pageState = 2;
+                      });
+                    },
+                    child: OutlineBtn(
+                      btnText: "Create New Account",
+                    ),
                   )
                 ],
               ),
             ],
           ),
         ),
-      )
-    ]);
+        AnimatedContainer(
+          height: _registerHeight,
+          padding: EdgeInsets.all(32),
+          curve: Curves.fastLinearToSlowEaseIn,
+          duration: Duration(milliseconds: 1000),
+          transform: Matrix4.translationValues(0, _registerYOffset, 1),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25), topRight: Radius.circular(25))),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(bottom: 20),
+                    child: Text(
+                      "Create a New Account",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  InputWithIcon(
+                    icon: Icons.email,
+                    hint: "Enter Email...",
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  InputWithIcon(
+                    icon: Icons.vpn_key,
+                    hint: "Enter Password...",
+                  )
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  PrimaryButton(
+                    btnText: "Create Account",
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _pageState = 1;
+                      });
+                    },
+                    child: OutlineBtn(
+                      btnText: "Back To Login",
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class InputWithIcon extends StatefulWidget {
+  final IconData icon;
+  final String hint;
+  InputWithIcon({this.icon, this.hint});
+
+  @override
+  _InputWithIconState createState() => _InputWithIconState();
+}
+
+class _InputWithIconState extends State<InputWithIcon> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: Color(0xFFBC7C7C7), width: 2),
+          borderRadius: BorderRadius.circular(50)),
+      child: Row(
+        children: <Widget>[
+          Container(
+              width: 60,
+              child: Icon(
+                widget.icon,
+                size: 20,
+                color: Color(0xFFBB9B9B9),
+              )),
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 20),
+                  border: InputBorder.none,
+                  hintText: widget.hint),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
 
 class PrimaryButton extends StatefulWidget {
-  @override
-  _PrimaryButtonState createState() => _PrimaryButtonState();
-
   final String btnText;
   PrimaryButton({this.btnText});
+
+  @override
+  _PrimaryButtonState createState() => _PrimaryButtonState();
 }
 
 class _PrimaryButtonState extends State<PrimaryButton> {
@@ -270,7 +374,7 @@ class _PrimaryButtonState extends State<PrimaryButton> {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          color: Color(0xFFB583d72), borderRadius: BorderRadius.circular(50)),
+          color: Color(0xFFB583D72), borderRadius: BorderRadius.circular(50)),
       padding: EdgeInsets.all(20),
       child: Center(
         child: Text(
@@ -285,6 +389,7 @@ class _PrimaryButtonState extends State<PrimaryButton> {
 class OutlineBtn extends StatefulWidget {
   final String btnText;
   OutlineBtn({this.btnText});
+
   @override
   _OutlineBtnState createState() => _OutlineBtnState();
 }
@@ -294,16 +399,13 @@ class _OutlineBtnState extends State<OutlineBtn> {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          border: Border.all(
-            color: Color(0xFFB583d72),
-            width: 2,
-          ),
+          border: Border.all(color: Color(0xFFB583D72), width: 2),
           borderRadius: BorderRadius.circular(50)),
       padding: EdgeInsets.all(20),
       child: Center(
         child: Text(
           widget.btnText,
-          style: TextStyle(color: Color(0xFFB583d72), fontSize: 16),
+          style: TextStyle(color: Color(0xFFB40284A), fontSize: 16),
         ),
       ),
     );
